b8156fd99a0da8ef3b990bb75e4238ffa4a2d0e6
Margaret Kenda <margaret.kenda@yahoo.com>
2022-09-22 11:27:47 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import About from './components/About';
+import Header from './components/Header';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Feedback from './components/Feedback';
+import Services from './components/Services';
+import Values from './components/Values';
+import Pricing from './components/Pricing';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Home/>
+      <About/>
+      <Values/>
+      <Services/>
+      <Portfolio/>
+      <Feedback/>
+      <Pricing/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Images1, Images2 } from './Images'
+
+function Portfolio() {
+  return (
+    <div>
+      <div id="portfolio" className="container-fluid text-center bg-grey">
+        <h2>Portfolio</h2>
+        <br />
+        <h4>What we have created</h4>
+        <div className="row text-center">
+          <div className="col-sm-4">
+            <div className="thumbnail">
+              <p>
+                <Images1 />
+                <strong>Paris</strong>
+              </p>
+              <p>Yes, we built Paris</p>
+            
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="thumbnail">
+              <p>
+                <Images2 />
+                <strong>New York</strong>
+              </p>
+              <p>We built New York</p>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="thumbnail">
+              <p>
+                <strong>San Francisco</strong>
+              </p>
+              <p>Yes, San Fran is ours</p>
+            </div>
+          </div>
+
+          <h2>What our customers say</h2>
+        </div>
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
